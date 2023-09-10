0bdd85a1555aab6016c0d3aa4791c392e05b6d60
Gabriel Terwesten <gabriel@terwesten.net>
2023-09-10 14:57:39 +0200

    Add Flutter example and tests

---
+++ example/flutter_example/integration_test/sqlite_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_example/sqlite.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('SQLite can be used in Flutter app', (widgetTester) async {
+    useSqlite();
+  });
+}
---
+++ example/flutter_example/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_example/sqlite.dart';
+
+void main() {
+  useSqlite();
+}
